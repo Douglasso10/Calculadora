77e3ed1ba2c9731794ba0ea90b557cb63eab08d1
Douglasso10 <douglas_soares15@hotmail.com>
2022-11-24 22:53:46 -0300

    começando a criação do JS

---
+++ script.js
@@ -0,0 +1,3 @@
+const operacaoAnterior = document.querySelector('#operacao_anterior')
+const operacaoAtual = document.querySelector('#operacao_atual')
+const botoes = document.querySelectorAll('#conteiner_botoes button')
